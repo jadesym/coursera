369cc516159b3c0677591d76b7f3bb9ef412c939
Kevin Fu <kevin.fu808@gmail.com>
2019-02-17 15:21:38 -0800

    Bumped score to 78

--- bitcoin-and-crypto/wk4-consensus/CompliantNode.java
+++ bitcoin-and-crypto/wk4-consensus/CompliantNode.java
@@ -1,67 +1,120 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
-    private final double _graphConnectivityProbability;
-    private final double _mailiciousNodeProbability;
-    private final double _transactionsCommunicatedProbability;
     private final int _numRounds;
 
     private Set<Integer> _followees = new HashSet<>();
-    private Set<Transaction> _validTransactions = new HashSet<>();
+    private Set<Transaction> _originalTransactions = new HashSet<>();
     private int _roundsCompleted = 0;
-    private Map<Transaction, Integer> _lastRoundTransactions;
+    private Map<Integer, Set<Transaction>> _lastRoundTransactions;
+//    private Map<Integer, Boolean> _transactionsHaveChanged;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-      _graphConnectivityProbability = p_graph;
-      _mailiciousNodeProbability = p_malicious;
-      _transactionsCommunicatedProbability = p_txDistribution;
       _numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+//        _transactionsHaveChanged = new HashMap<>();
+
         for (int i = 0; i < followees.length; i++) {
           if (followees[i]) {
             _followees.add(i);
-          };
+//            _transactionsHaveChanged.put(i, false);
+          }
         }
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-      for (Transaction transaction : pendingTransactions) {
-        _validTransactions.add(transaction);
-      }
+        _originalTransactions.addAll(pendingTransactions);
     }
 
     public Set<Transaction> sendToFollowers() {
       if (_roundsCompleted >= _numRounds) {
+        Set<Integer> validNodes = _lastRoundTransactions == null ? new HashSet<>(_followees)
+                : _followees
+                .stream()
+                .filter(nodeId -> _lastRoundTransactions.containsKey(nodeId))
+                .collect(Collectors.toSet());
+
+        Map<Transaction, Integer> transactionCounts = new HashMap<>();
+
+        if (_lastRoundTransactions != null) {
+            _lastRoundTransactions.forEach((nodeId, transactionSet) -> {
+                for (Transaction transaction : transactionSet) {
+                    transactionCounts.put(transaction, transactionCounts.getOrDefault(transaction, 0) + 1);
+                }
+            });
+        }
+
         Set<Transaction> consensusTransactions = new HashSet<>();
-        _lastRoundTransactions
+        transactionCounts
           .entrySet()
           .stream()
-          .filter(entry -> entry.getValue() > _followees.size() / 2)
+          .filter(entry -> entry.getValue() > validNodes.size() / 2)
           .forEach(entry -> consensusTransactions.add(entry.getKey()));
         return consensusTransactions;
       }
 
       _roundsCompleted++;
 
-      return _validTransactions;
+      Set<Transaction> finalTransactions = new HashSet<>(_originalTransactions);
+
+      if (_lastRoundTransactions != null) {
+          _lastRoundTransactions.values()
+                  .stream()
+                  .flatMap(Collection::stream)
+                  .forEach(finalTransactions::add);
+      }
+
+      return finalTransactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-      _lastRoundTransactions = new HashMap<>();
+      Map<Integer, Set<Transaction>> newRoundTransactions = getRoundTransactions(candidates);
+
+      if (_lastRoundTransactions == null) {
+        _lastRoundTransactions = newRoundTransactions;
+        return;
+      }
+
+      for (Integer nodeId : _lastRoundTransactions.keySet()) {
+          if (!newRoundTransactions.containsKey(nodeId)) {
+              _followees.remove(nodeId);
+              continue;
+          }
+
+          Set<Transaction> lastRoundNodeTransactions = _lastRoundTransactions.get(nodeId);
+          Set<Transaction> newRoundNodeTransactions = newRoundTransactions.get(nodeId);
+
+          lastRoundNodeTransactions.stream()
+                  .filter(lastRoundNodeTransaction -> !newRoundNodeTransactions.contains(lastRoundNodeTransaction))
+                  .forEach(lastRoundNodeTransaction -> _followees.remove(nodeId));
+
+//          newRoundNodeTransactions.stream()
+//                  .filter(newRoundNodeTransaction -> !lastRoundNodeTransactions.contains(newRoundNodeTransaction))
+//                  .forEach(newRoundTransaction -> _transactionsHaveChanged.put(nodeId, true));
+      }
+
+      _lastRoundTransactions = newRoundTransactions;
+    }
+
+    private Map<Integer, Set<Transaction>> getRoundTransactions(Set<Candidate> candidates) {
+      Map<Integer, Set<Transaction>> newRoundTransactions = new HashMap<>();
+
       for (Candidate candidate : candidates) {
         Transaction candidateTransaction = candidate.tx;
         Integer candidateSender = candidate.sender;
 
-        _validTransactions.add(candidateTransaction);
+        if (!newRoundTransactions.containsKey(candidateSender)) {
+          newRoundTransactions.put(candidateSender, new HashSet<>());
+        }
 
-        _lastRoundTransactions.put(candidateTransaction, _lastRoundTransactions.getOrDefault(candidateTransaction, 0) + 1);
+        newRoundTransactions.get(candidateSender)
+          .add(candidateTransaction);
       }
+
+      return newRoundTransactions;
     }
 }
