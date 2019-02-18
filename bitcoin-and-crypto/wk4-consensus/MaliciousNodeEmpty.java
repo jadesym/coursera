e632656520d6fac86bd0b9ee72403408901139cd
Kevin Fu <kevin.fu808@gmail.com>
2019-02-17 20:25:00 -0800

    Adding other malicious node possibilities

--- bitcoin-and-crypto/wk4-consensus/MaliciousNodeEmpty.java
+++ bitcoin-and-crypto/wk4-consensus/MaliciousNodeEmpty.java
@@ -2,11 +2,9 @@ import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
 
-public class MaliciousNode implements Node {
+public class MaliciousNodeEmpty implements Node {
 
-    private Set<Transaction> _originalTransactions;
-
-    public MaliciousNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+    public MaliciousNodeEmpty(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
     }
 
     public void setFollowees(boolean[] followees) {
@@ -14,11 +12,11 @@ public class MaliciousNode implements Node {
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        _originalTransactions = pendingTransactions;
+        return;
     }
 
     public Set<Transaction> sendToFollowers() {
-        return new HashSet<>(_originalTransactions);
+        return new HashSet<Transaction>();
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
